3931ecef58f9d8d6d06f4022de1194a6aa9b5335
filippo199600 <pippotartaglia@gmail.com>
2023-09-06 15:21:27 +0200

    print on console single email

--- js/main.js
+++ js/main.js
@@ -6,4 +6,12 @@ createApp({
       title: "title vue",
     };
   },
+
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        console.log(response.data.response);
+      });
+  },
 }).mount("#app");
